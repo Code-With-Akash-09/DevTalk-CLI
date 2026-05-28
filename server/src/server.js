aa0bf031d6921e1092f752dc816997f63757baa2
Akash09082001 <aakashl@letsupgrade.in>
2026-05-28 17:31:34 +0530

    feat: add cron service for health check and implement health endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -12,6 +12,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "ok",
+    })
+})
+
 app.use("/v1/auth", authRouter)
 
 getDb()
